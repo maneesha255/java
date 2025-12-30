0afa345d4935975b2c134b976a4eeeb85cfe4a4b
maneesha255 <maneeshanayapamu@gmail.com>
2025-12-30 15:30:56 +0530

    Add files via upload

--- manee.1.py
+++ manee.1.py
@@ -1,15 +1,16 @@
 def differenceofSum(n,m):
- sum_divisible=0
- sum_not_divisible=0
- for i in range(1,m+1):
-     if i%n==0:
-         sum_divisible+=i
-        else:
+   sum_divisible=0
+   sum_not_divisible=0
+   for i in range(1,m+1):
+       if i%n==0:
+           sum_divisible+=i
+       else:
             sum_not_divisible+=i
-        return sum_not_divisible_sum_divisible
+            return sum_not_divisible_sum_divisible
 
-# Sample inputs
+
+#Sample inputs
 n=int(input("Enter n:"))
 m=int(input("Enter m:"))
-result =different of sum(n,m)
+result =differentofsum(n,m)
 print("Difference=",result)
